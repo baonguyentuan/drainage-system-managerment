9faf4e2d69ab5259ff0775f6642c6aa48c20f02f
baonguyentuan <nguyentuanbao.1994@gmail.com>
2023-12-14 16:06:13 +0700

    import text to cad

--- src/components/TextTool/ImportCad.tsx
+++ src/components/TextTool/ImportCad.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Button, Col, Form, Input, InputNumber, Row, Select } from 'antd';
 import { string } from 'yup';
 import { log } from 'console';
+import configDxf from '../../untils/config/configDxf';
 const { Option } = Select;
 const { TextArea } = Input;
 type Props = {}
 const baseOrdinate = 1000
-const baseDistance = 1000
+const baseDistance = 50
 let renderValue = (value: string | null) => {
     if (value) {
         return value.trim()
@@ -57,47 +58,63 @@ const ImportCad = (props: Props) => {
             <Button onClick={() => {
                 let arr: any[]
                 let contentRender: string
+                let finalContent: string
                 arr = []
                 contentRender = ``
-                console.log(doc.split('\n'));
-
                 doc.split('\n').map((text, indexText) => {
                     let arrDoc: string[]
                     arrDoc = []
                     if (text !== "") {
                         text.split('\t').map((textItem, indexTextItem) => {
                             contentRender += `
-                    0
-                    TEXT
-                    5
-                    C644
-                    330
-                    1F
-                    100
-                    AcDbEntity
-                      8
-                    !CBKT text
-                    100
-                    AcDbText
-                     10
-                     ${baseOrdinate + indexTextItem * baseDistance}
-                     20
-                     ${baseOrdinate + indexText * baseDistance}
-                     30
-                     0
-                     40
-                    7
-                      1
-                    ${renderValue(textItem)}
-                     50
-                    0
-                      7
-                    Q
-                    `
-
+0
+TEXT
+    5
+B${indexTextItem}${indexText}
+330
+1F
+100
+AcDbEntity
+    8
+0
+100
+AcDbText
+    10
+${baseOrdinate + baseDistance * indexTextItem}
+    20
+${baseOrdinate + baseDistance * indexText / 2}
+    30
+0.0
+    40
+4.022976867721411
+    1
+${textItem}
+100
+AcDbText`
                         })
                     }
                 })
+                finalContent = configDxf.header + contentRender + configDxf.footer
+                const writeToTextFile = (text: string, fileName: string) => {
+                    let textFile: string | null;
+                    textFile = null;
+                    const makeTextFile = (text: string) => {
+                        const data = new Blob([text], {
+                            type: 'text/plain',
+                        });
+                        if (textFile !== null) {
+                            window.URL.revokeObjectURL(textFile);
+                        }
+                        textFile = window.URL.createObjectURL(data);
+                        return textFile;
+                    };
+                    const link = document.createElement('a');
+                    link.setAttribute('download', fileName);
+                    link.href = makeTextFile(text);
+                    link.click();
+                };
+                writeToTextFile(finalContent, 'output.dxf');
+
 
             }}>Xuất sang file CAD</Button>
         </div>
