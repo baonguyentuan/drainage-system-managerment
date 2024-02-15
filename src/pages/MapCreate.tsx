0269f037e78b40dbc7ab283a7f756bae151e0309
baonguyen <nguyentuanbao.1994@gmail.com>
2024-02-15 16:28:45 +0700

    leaflet map

--- src/pages/MapCreate.tsx
+++ src/pages/MapCreate.tsx
@@ -1,52 +1,22 @@
 import React from "react";
-import {
-  FeatureGroup,
-  MapContainer,
-  Marker,
-  Polyline,
-  Popup,
-  TileLayer,
-} from "react-leaflet";
-import { EditControl } from "react-leaflet-draw";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 type Props = {};
 const MapCreate = (props: Props) => {
-  // const position = [21.005, 105.89];
   return (
-    <div className="">
+    <div>
       <MapContainer
         className="w-screen h-screen"
+        zoomControl={false}
         center={[21.019098, 105.841385]}
         zoom={15}
-        maxZoom={30}
-        minZoom={5}
         scrollWheelZoom={true}
       >
         <TileLayer
           url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+          maxZoom={20}
           subdomains={["mt1", "mt2", "mt3"]}
         />
-        <Marker position={[21.019098, 105.841383]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Polyline
-          pathOptions={{ color: "lime", weight: 5, }}
-          positions={[
-            [21.019098, 105.841385],
-            [21.019096, 105.841387],
-            [21.019100, 105.841389],
-          ]}
-        />
-        <FeatureGroup>
-          <EditControl
-            position="topright"
-            draw={{
-              
-            }}
-          />
-        </FeatureGroup>
       </MapContainer>
     </div>
   );
