0524381c1d89d6615f9336761c4bc249e4673244
baonguyentuan <nguyentuanbao.1994@gmail.com>
2024-03-13 00:03:06 +0700

    update

--- src/pages/CaptureMap.tsx
+++ src/pages/CaptureMap.tsx
@@ -1,34 +1,27 @@
-import { LatLngExpression, LatLngTuple } from "leaflet";
-import React, { useEffect, useRef, useState } from "react";
-import {
-  MapContainer,
-  Rectangle,
-  TileLayer,
-  useMap,
-  useMapEvents,
-} from "react-leaflet";
+import { LatLngTuple } from "leaflet";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { saveAs } from "file-saver";
 import domtoimage from "dom-to-image";
-import { Button, Form, Modal, Radio, Select } from "antd";
+import { Form, Modal, Radio, Select } from "antd";
 type Props = {};
-
+interface ImgRegionItem {
+  name: string;
+  northWest: LatLngTuple;
+  southEast: LatLngTuple;
+}
 const CaptureMap = (props: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(true);
   let [mapType, setMapType] = useState<number>(1);
   let [position, setPosition] = useState<LatLngTuple>([21.029098, 105.842385]);
+  let [region, setRegion] = useState<number>(0);
+  let [imgRegion, setImgRegion] = useState<ImgRegionItem[]>([]);
   let bound: LatLngTuple[] = [
     [21.019098, 105.841385],
     [21.029098, 105.851385],
   ];
   let dx = bound[0][0] - bound[1][0];
   let dy = bound[0][1] - bound[1][1];
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
   const handleMapView = async () => {
     await setIsModalOpen(false);
     await setPosition([position[0] + dx, position[1] + dy]);
@@ -39,6 +32,13 @@ const CaptureMap = (props: Props) => {
         height: window.innerHeight,
       });
       await saveAs(blob, "map.png");
+      let tempImgRegion = [...imgRegion];
+      tempImgRegion.push({
+        name: `${region}-${imgRegion.length + 1}`,
+        northWest: bound[0],
+        southEast: bound[1],
+      });
+      setImgRegion(tempImgRegion);
     }
     if (position[0] < 21.1) {
       let btnSnap = document.getElementById("btnSnapshot");
@@ -59,7 +59,6 @@ const CaptureMap = (props: Props) => {
         [newBound.getNorthWest().lat, newBound.getNorthWest().lng],
         [newBound.getSouthEast().lat, newBound.getSouthEast().lng],
       ];
-      console.log(bound);
     }, []);
     return null;
   }
@@ -95,7 +94,18 @@ const CaptureMap = (props: Props) => {
               options={[
                 { value: 0, label: "XNNT" },
                 { value: 1, label: "XN1" },
+                { value: 2, label: "XN2" },
+                { value: 3, label: "XN3" },
+                { value: 4, label: "XN4" },
+                { value: 5, label: "XN5" },
+                { value: 6, label: "XN6" },
+                { value: 7, label: "XN7" },
+                { value: 7, label: "XN8" },
               ]}
+              value={region}
+              onChange={(value) => {
+                setRegion(value);
+              }}
             />
           </Form.Item>
         </Form>
