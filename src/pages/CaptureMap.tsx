474bd8543bb26cd62f10cb9c35dfd45d4a7af36a
baonguyentuan <nguyentuanbao.1994@gmail.com>
2024-03-13 18:18:37 +0700

    update

--- src/pages/CaptureMap.tsx
+++ src/pages/CaptureMap.tsx
@@ -14,33 +14,34 @@ const CaptureMap = (props: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(true);
   let [mapType, setMapType] = useState<number>(1);
   let [position, setPosition] = useState<LatLngTuple>([21.029098, 105.842385]);
-  let [region, setRegion] = useState<number>(0);
-  let [imgRegion, setImgRegion] = useState<ImgRegionItem[]>([]);
+  let [region, setRegion] = useState<string>("XNNT");
+  let imgRegion: ImgRegionItem[] = [];
   let bound: LatLngTuple[] = [
     [21.019098, 105.841385],
     [21.029098, 105.851385],
   ];
-  let dx = bound[0][0] - bound[1][0];
-  let dy = bound[0][1] - bound[1][1];
+  console.log(imgRegion);
+
+  let dx = Math.abs(bound[0][0] - bound[1][0]);
+  let dy = Math.abs(bound[0][1] - bound[1][1]);
   const handleMapView = async () => {
+    console.log(dx, dy);
     await setIsModalOpen(false);
-    await setPosition([position[0] + dx, position[1] + dy]);
+    await setPosition([position[0] + dx, position[1]]);
     const mapCanvas = document.getElementById("mapMain");
     if (mapCanvas) {
       const blob = await domtoimage.toBlob(mapCanvas, {
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: mapCanvas.clientWidth,
+        height: mapCanvas.clientHeight,
       });
-      await saveAs(blob, "map.png");
-      let tempImgRegion = [...imgRegion];
-      tempImgRegion.push({
+      await saveAs(blob, `${region}-${imgRegion.length + 1}.png`);
+      imgRegion.push({
         name: `${region}-${imgRegion.length + 1}`,
         northWest: bound[0],
         southEast: bound[1],
       });
-      setImgRegion(tempImgRegion);
     }
-    if (position[0] < 21.1) {
+    if (position[0] < 21.05) {
       let btnSnap = document.getElementById("btnSnapshot");
       btnSnap?.click();
     } else {
@@ -56,8 +57,8 @@ const CaptureMap = (props: Props) => {
     useEffect(() => {
       let newBound = map.getBounds();
       bound = [
-        [newBound.getNorthWest().lat, newBound.getNorthWest().lng],
-        [newBound.getSouthEast().lat, newBound.getSouthEast().lng],
+        [newBound.getSouthWest().lat, newBound.getSouthWest().lng],
+        [newBound.getNorthEast().lat, newBound.getNorthEast().lng],
       ];
     }, []);
     return null;
@@ -92,15 +93,15 @@ const CaptureMap = (props: Props) => {
             <Select
               className="w-full"
               options={[
-                { value: 0, label: "XNNT" },
-                { value: 1, label: "XN1" },
-                { value: 2, label: "XN2" },
-                { value: 3, label: "XN3" },
-                { value: 4, label: "XN4" },
-                { value: 5, label: "XN5" },
-                { value: 6, label: "XN6" },
-                { value: 7, label: "XN7" },
-                { value: 7, label: "XN8" },
+                { value: "XNNT", label: "XNNT" },
+                { value: "XN1", label: "XN1" },
+                { value: "XN2", label: "XN2" },
+                { value: "XN3", label: "XN3" },
+                { value: "XN4", label: "XN4" },
+                { value: "XN5", label: "XN5" },
+                { value: "XN6", label: "XN6" },
+                { value: "XN7", label: "XN7" },
+                { value: "XN8", label: "XN8" },
               ]}
               value={region}
               onChange={(value) => {
@@ -136,7 +137,7 @@ const CaptureMap = (props: Props) => {
         onClick={() => {
           setTimeout(() => {
             handleMapView();
-          }, 3000);
+          }, 2000);
         }}
       ></button>
     </div>
