542047ad9aa946d9c8fc074fbbfbcb6162993326
baonguyentuan <nguyentuanbao.1994@gmail.com>
2024-03-15 00:16:06 +0700

    update

--- src/pages/CaptureMap.tsx
+++ src/pages/CaptureMap.tsx
@@ -1,66 +1,124 @@
-import { LatLngTuple } from "leaflet";
+import { LatLng, LatLngTuple } from "leaflet";
 import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Rectangle,
+  TileLayer,
+  useMap,
+} from "react-leaflet";
 import { saveAs } from "file-saver";
 import domtoimage from "dom-to-image";
-import { Form, Modal, Radio, Select } from "antd";
+import {
+  Button,
+  Col,
+  Form,
+  Modal,
+  Radio,
+  Row,
+  Select,
+  Slider,
+  Space,
+} from "antd";
+import { convertWgs84ToVn2000 } from "../untils/operate/vn2000andWgs84/wgs84toVn2000";
+import { DxfWriter, Units, point2d, point3d } from "@tarikjabiri/dxf";
+import { CheckCircleOutlined } from "@ant-design/icons";
 type Props = {};
 interface ImgRegionItem {
   name: string;
-  northWest: LatLngTuple;
-  southEast: LatLngTuple;
+  pointCenter: LatLngTuple;
 }
+let snapStatus: boolean = false;
 const CaptureMap = (props: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(true);
   let [mapType, setMapType] = useState<number>(1);
   let [position, setPosition] = useState<LatLngTuple>([21.029098, 105.842385]);
-  let [region, setRegion] = useState<string>("XNNT");
-  let imgRegion: ImgRegionItem[] = [];
-  let bound: LatLngTuple[] = [
-    [21.019098, 105.841385],
-    [21.029098, 105.851385],
-  ];
-  console.log(imgRegion);
-
+  let [region, setRegion] = useState<string>("XNTK");
+  let [zoomMap, setZoomMap] = useState<number>(16);
+  let [regionCapture, setRegionCapture] = useState<LatLngTuple[]>([]);
+  let [imgRegion, setImgRegion] = useState<ImgRegionItem[]>([]);
+  let [bound, setBound] = useState<LatLngTuple[]>([
+    [0, 0],
+    [0, 0],
+  ]);
+  let [sizeMap, setSizeMap] = useState<number[]>([0, 0]);
   let dx = Math.abs(bound[0][0] - bound[1][0]);
   let dy = Math.abs(bound[0][1] - bound[1][1]);
   const handleMapView = async () => {
-    console.log(dx, dy);
-    await setIsModalOpen(false);
-    await setPosition([position[0] + dx, position[1]]);
-    const mapCanvas = document.getElementById("mapMain");
-    if (mapCanvas) {
-      const blob = await domtoimage.toBlob(mapCanvas, {
-        width: mapCanvas.clientWidth,
-        height: mapCanvas.clientHeight,
-      });
-      await saveAs(blob, `${region}-${imgRegion.length + 1}.png`);
-      imgRegion.push({
-        name: `${region}-${imgRegion.length + 1}`,
-        northWest: bound[0],
-        southEast: bound[1],
-      });
+    if (position[1] <= regionCapture[1][1] - dy * 0.1) {
+      setPosition([position[0], position[1] + dy * 0.95]);
+      const mapCanvas = document.getElementById("mapMain");
+      if (mapCanvas) {
+        const blob = await domtoimage.toBlob(mapCanvas, {
+          width: mapCanvas.clientWidth,
+          height: mapCanvas.clientHeight,
+        });
+        await saveAs(blob, `${region}-${imgRegion.length + 1}.png`);
+        let newLst = imgRegion;
+        newLst.push({
+          name: `${region}-${imgRegion.length + 1}`,
+          pointCenter: position,
+        });
+        await setImgRegion(newLst);
+      }
+    } else {
+      setPosition([position[0] - dx * 0.95, regionCapture[0][1]]);
+      const mapCanvas = document.getElementById("mapMain");
+      if (mapCanvas) {
+        const blob = await domtoimage.toBlob(mapCanvas, {
+          width: mapCanvas.clientWidth,
+          height: mapCanvas.clientHeight,
+        });
+        await saveAs(blob, `${region}-${imgRegion.length + 1}.png`);
+        let newLst = imgRegion;
+        newLst.push({
+          name: `${region}-${imgRegion.length + 1}`,
+          pointCenter: position,
+        });
+        await setImgRegion(newLst);
+      }
     }
-    if (position[0] < 21.05) {
+    if (position[0] <= regionCapture[1][0] + dx * 0.1) {
+      snapStatus = false;
+      alert("Hoàn thành");
+    }
+    if (snapStatus) {
       let btnSnap = document.getElementById("btnSnapshot");
       btnSnap?.click();
     } else {
       await setIsModalOpen(true);
     }
   };
+  console.log(sizeMap);
 
-  function CaptureRectangle({ pos }: { pos: LatLngTuple }) {
+  function CaptureView({ pos, zoom }: { pos: LatLngTuple; zoom: number }) {
     const map = useMap();
+    map.addEventListener("click", async (e) => {
+      await setSizeMap([map.getSize().x, map.getSize().y]);
+      if (regionCapture.length === 0) {
+        await setRegionCapture([[e.latlng.lat, e.latlng.lng]]);
+      } else if (regionCapture.length === 1) {
+        let newRegion = regionCapture;
+        newRegion.push([e.latlng.lat, e.latlng.lng]);
+        await setRegionCapture(newRegion);
+        let newBound = map.getBounds();
+        await setBound([
+          [newBound.getSouthWest().lat, newBound.getSouthWest().lng],
+          [newBound.getNorthEast().lat, newBound.getNorthEast().lng],
+        ]);
+
+        await setPosition([
+          regionCapture[0][0] - dx * 0.4,
+          regionCapture[0][1],
+        ]);
+        await setIsModalOpen(true);
+      }
+    });
+
     useEffect(() => {
+      map.setZoom(zoom);
       map.setView(pos);
-    }, pos);
-    useEffect(() => {
-      let newBound = map.getBounds();
-      bound = [
-        [newBound.getSouthWest().lat, newBound.getSouthWest().lng],
-        [newBound.getNorthEast().lat, newBound.getNorthEast().lng],
-      ];
-    }, []);
+    }, [map, pos, zoom]);
     return null;
   }
   return (
@@ -68,10 +126,9 @@ const CaptureMap = (props: Props) => {
       <Modal
         title="Basic Modal"
         open={isModalOpen}
-        onOk={() => {
-          setTimeout(() => {
-            handleMapView();
-          }, 3000);
+        onOk={async () => {
+          snapStatus = true;
+          handleMapView();
         }}
         okType="danger"
         okText={"Chụp"}
@@ -89,25 +146,81 @@ const CaptureMap = (props: Props) => {
               }}
             />
           </Form.Item>
-          <Form.Item label="Địa bàn">
-            <Select
-              className="w-full"
-              options={[
-                { value: "XNNT", label: "XNNT" },
-                { value: "XN1", label: "XN1" },
-                { value: "XN2", label: "XN2" },
-                { value: "XN3", label: "XN3" },
-                { value: "XN4", label: "XN4" },
-                { value: "XN5", label: "XN5" },
-                { value: "XN6", label: "XN6" },
-                { value: "XN7", label: "XN7" },
-                { value: "XN8", label: "XN8" },
-              ]}
-              value={region}
-              onChange={(value) => {
-                setRegion(value);
-              }}
-            />
+          <Form.Item label="Độ phóng">
+            <Row className="items-center">
+              <Col span={20}>
+                <Slider
+                  className="w-4/5"
+                  max={18}
+                  min={14}
+                  value={zoomMap}
+                  onChange={(value) => {
+                    setZoomMap(value);
+                  }}
+                />
+              </Col>
+              <Col span={4}>
+                <p>{zoomMap}</p>
+              </Col>
+            </Row>
+          </Form.Item>
+          <Form.Item>
+            <Space>
+              <Button
+                disabled={regionCapture.length === 2 ? false : true}
+                onClick={async () => {
+                  console.log(imgRegion);
+                  const dxf = new DxfWriter();
+
+                  dxf.setUnits(Units.Meters);
+                  imgRegion.forEach((img, index) => {
+                    let ordinateBotLeft = convertWgs84ToVn2000([
+                      img.pointCenter[0] - dx / 2,
+                      img.pointCenter[1] - dy / 2,
+                      0,
+                    ]);
+                    let ordinateTopRight = convertWgs84ToVn2000([
+                      img.pointCenter[0] + dx / 2,
+                      img.pointCenter[1] + dy / 2,
+                      0,
+                    ]);
+                    dxf.addImage(
+                      "..\\" + img.name + ".png",
+                      img.name,
+                      point3d(ordinateBotLeft[1], ordinateBotLeft[0]),
+                      sizeMap[0],
+                      sizeMap[1],
+                      Math.abs(ordinateBotLeft[1] - ordinateTopRight[1]),
+                      0
+                    );
+                  });
+                  const dxfString = dxf.stringify();
+                  console.log(dxfString);
+                  let newBlob = new Blob([dxfString], {
+                    type: "text/plain;charset=utf-8",
+                  });
+                  await saveAs(newBlob, `${region}.dxf`);
+                }}
+              >
+                Lưu tệp
+              </Button>
+              <Button
+                onClick={() => {
+                  setIsModalOpen(false);
+                }}
+              >
+                Chọn vùng chụp
+              </Button>
+            </Space>
+            <div>
+              {regionCapture.map((corner, index) => {
+                return (
+                  <p key={index}>
+                    {corner[0]} | {corner[1]}
+                  </p>
+                );
+              })}
+            </div>
           </Form.Item>
         </Form>
       </Modal>
@@ -118,9 +231,10 @@ const CaptureMap = (props: Props) => {
         zoomControl={false}
         center={[position[0], position[1]]}
         zoom={16}
-        scrollWheelZoom={true}
+        scrollWheelZoom={false}
       >
         <TileLayer
+          attribution="Xí nghiệp khảo sát thiết kế"
           url={
             mapType === 1
               ? "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
@@ -129,7 +243,10 @@ const CaptureMap = (props: Props) => {
           // maxZoom={20}
           subdomains={["mt1", "mt2", "mt3"]}
         />
-        <CaptureRectangle pos={position} />
+        <CaptureView pos={position} zoom={zoomMap} />
+        {regionCapture.map((point, index) => {
+          return <Marker key={index} position={point} />;
+        })}
       </MapContainer>
       <button
         className="opacity-0 fixed"
@@ -137,7 +254,7 @@ const CaptureMap = (props: Props) => {
         onClick={() => {
           setTimeout(() => {
             handleMapView();
-          }, 2000);
+          }, 3000);
         }}
       ></button>
     </div>
